0cc72a91bd6692c3542ea60ab592371d4a0ddbbd
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-26 21:34:15 -0700

    confirm that HostPortAllocator skips unavailable ports

--- spec/models/host_port_allocator_spec.rb
+++ spec/models/host_port_allocator_spec.rb
@@ -6,12 +6,19 @@ describe HostPortAllocator do
   before { FileUtils.rm_rf('/tmp/host_port_counter') }
   let(:subject) { described_class.new('/tmp', 10000, 10004) }
   it 'should allocate consecutive ports and wrap around' do
-    expect(subject.send(:allocate_next_port)).to eq(10000)
-    expect(subject.send(:allocate_next_port)).to eq(10001)
-    expect(subject.send(:allocate_next_port)).to eq(10002)
-    expect(subject.send(:allocate_next_port)).to eq(10003)
-    expect(subject.send(:allocate_next_port)).to eq(10004)
-    expect(subject.send(:allocate_next_port)).to eq(10000)
+    expect(subject.allocate_next_port).to eq(10000)
+    expect(subject.allocate_next_port).to eq(10001)
+    expect(subject.allocate_next_port).to eq(10002)
+    expect(subject.allocate_next_port).to eq(10003)
+    expect(subject.allocate_next_port).to eq(10004)
+    expect(subject.allocate_next_port).to eq(10000)
+  end
+
+  it 'should skip unavailable ports' do
+    expect(subject).to receive(:port_available?).with(10000).and_return(false)
+    expect(subject).to receive(:port_available?).with(10001).and_return(false)
+    expect(subject).to receive(:port_available?).with(10002).and_return(true)
+    expect(subject.allocate_next_port).to eq(10002)
   end
 
   it 'should raise NoAvailablePort if cannot find a port' do
