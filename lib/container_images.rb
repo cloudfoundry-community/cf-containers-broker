81244b4afafe0c6c41b2b1b2482ad1e2930f4bab
Justin Carter <jcarter@starkandwayne.com>
2016-03-17 12:42:09 +0100

    Add bin/update_container_images

--- lib/container_images.rb
+++ lib/container_images.rb
@@ -11,4 +11,11 @@ module ContainerImages
       plan.container_manager.fetch_image
     end
   end
+
+  def update
+    Rails.logger.info('Updating containers to latest image')
+    Catalog.plans.each do |plan|
+      plan.container_manager.update_containers_to_latest_image
+    end
+  end
 end
