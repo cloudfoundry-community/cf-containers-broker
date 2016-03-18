5a4f3b6cdc25a4e16a937ea3fcec7f42580dd4a4
Justin Carter <jcarter@starkandwayne.com>
2016-03-18 09:49:52 +0100

    Rename update_container_images to update_all_containers

--- lib/container_lifecycle.rb
+++ lib/container_lifecycle.rb
@@ -2,13 +2,13 @@
 # Copyright (c) 2014 Pivotal Software, Inc. All Rights Reserved.
 require Rails.root.join('app/models/catalog')
 
-module ContainerImages
+module ContainerLifecycle
   extend self
 
-  def fetch
-    Rails.logger.info('Looking for container images at the Services Catalog')
+  def update_all
+    Rails.logger.info('Updating all tagged containers')
     Catalog.plans.each do |plan|
-      plan.container_manager.fetch_image
+      plan.container_manager.update_all_containers
     end
   end
 end
