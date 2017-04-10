0cceff2a728c3705c0389b066b402544abdccd9e
Dr Nic Williams <drnicwilliams@gmail.com>
2017-04-10 10:56:01 +1000

    $SKIP_FETCHING_IMAGES=true will skip loading images (e.g. if no network connection)

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # Fetch Containers Images
-echo "Fetching Containers Images..."
-bin/fetch_container_images
+if [[ "${SKIP_FETCHING_IMAGES:-X}" == "X" ]]; then
+  echo "Fetching Containers Images..."
+  bin/fetch_container_images
+else
+  echo "Skipping fetching container images."
+fi
 
 # Start CF-Containers-Broker
 echo "Starting CF-Containers-Broker..."
